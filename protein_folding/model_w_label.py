9d2c53c9de45fc2bd20981f9ffc8a8198c021ff1
ElanaSimon95 <ElanaSimon95@gmail.com>
2017-06-22 15:54:58 -0400

    reorganized and cleaned code

--- protein_folding/model_w_label.py
+++ protein_folding/model_w_label.py
@@ -5,12 +5,15 @@ class MultipleSequenceAlignment:
     def __init__(self, filename):
         self.filename = filename
 
-        self.alphabet = 'ACDEFGHIKLMNPQRSTVWY'
+        self.alphabet = 'ACDEFGHIKLMNPQRSTVWY*'
+        self.end_token = '*'
+
         self.acceptable_seq_alphabet = self.alphabet + '.-'
         self.alphabet_len = len(self.alphabet)
         self.alphabet_map = {s: i for i, s in enumerate(self.alphabet)}
+        self.rev_alphabet_map = {i: s for i, s in enumerate(self.alphabet)}
 
-        # Dictionary: key = seq_id, value = array of seq chars
+        # Dictionary: {species_id, sequence string}
         self.seqs = self._read_data()
 
         self.max_seq_len = max(len(seq) for seq in self.seqs.values())
@@ -33,24 +36,38 @@ class MultipleSequenceAlignment:
         self.train_seq_ids = list(set(self.seqs.keys()) - set(self.test_seq_ids))
 
 
-    def _add_clean_seq(self, curr_id, curr_seq):
-        clean_seq = []
+    def _add_sequence(self, curr_id, curr_seq):
+        """ Adds a given sequence to the sequence dictionary.
+
+        First it adds the 'end token' to the sequence then it checks if it
+        contains any characters other than traditional amino acids and gaps.
+        If it doesn't, then it adds this sequence to the sequences dictionary
+        """
+
+        encoded_seq = ''
+        #sequence = []
         ignore_this_seq = False
 
         for aa in curr_seq:
-            try:
-                aa_c = self.alphabet_map[aa]
-                clean_seq.append(aa_c)
-            except:
-                if aa.upper() not in self.acceptable_seq_alphabet:
-                    ignore_this_seq = True
+            # If it is a capital letter and in the alphabet add it to clean seq
+            if aa in self.alphabet_map:
+                encoded_seq += aa
+
+            # If the sequence includes non aa letters, ignore the sequence
+            elif aa.upper() not in self.acceptable_seq_alphabet:
+                ignore_this_seq = True
 
         if not ignore_this_seq:
-            self.sequences[curr_id] = clean_seq
+            self.sequences[curr_id] = encoded_seq
 
 
     def _read_data(self):
-        """ Reads in data from filename and returns a dict of only seqs without weird characters"""
+        """ Converts data into 
+
+        Reads in the a2m alignment file an converts it into a dictionary where
+        the keys are species ids and the values the sequences. And it only
+        includes sequences made of traditional amino acids.
+        """
 
         with open(self.filename)as f:
             current_sequence = ""
@@ -63,7 +80,7 @@ class MultipleSequenceAlignment:
                 # seen an entry before, return it first.
                 if line.startswith(">"):
                     if current_id is not None:
-                        self._add_clean_seq(current_id, current_sequence)
+                        self._add_sequence(current_id, current_sequence)
 
                     current_id = line.rstrip()[1:]
                     current_sequence = ""
@@ -72,53 +89,88 @@ class MultipleSequenceAlignment:
                     current_sequence += line.rstrip()
 
             # Also do not forget last entry in file
-            self._add_clean_seq(current_id, current_sequence)
+            self._add_sequence(current_id, current_sequence)
         
         return self.sequences
-    
+   
 
-    def _idx_to_one_hot(self, seq):
-        zero_padding = [0]*(self.max_seq_len - len(seq))
-        seq += zero_padding
+    def convert_to_output(self, seq):
+        return seq[1:] + self.end_token
+
+
+    def str_to_one_hot(self, seq):
+        """ Turns a sequence of amino acids into a one-hot encoded matrix.
+        """
+        
+        # Encode the string as an array of indices
+        encoded_seq = [self.alphabet_map[aa] for aa in seq]
 
         # Turn this into one hot encoding
         one_hot_seq = np.zeros((self.max_seq_len, self.alphabet_len))
-        one_hot_seq[np.arange(self.max_seq_len), seq] = 1
+        one_hot_seq[np.arange(len(encoded_seq)), encoded_seq] = 1
         return one_hot_seq
 
 
-    def next_batch(self, batch_size, test=False):
-        mb = np.zeros((batch_size, self.max_seq_len, self.alphabet_len))
+    def one_hot_to_str(self, seq):
+        """ Converts a one-hot encoded sequence back into an amino acid sequence.
 
-        for i in range(batch_size):  
+        Given a one-hot encoded seq of shape seq_len x alphabet_len this returns
+        a string representing the amino acid sequence the input represents
+        """
+
+        # Trim off the end (after the end token)
+        trimmed_seq = seq[:np.sum(seq)]
+        
+        encoded_seq = np.argmax(seq, 1)
+        decoded_seq = map(lambda idx: self.rev_alphabet_map[idx], encoded_seq)
+        return ''.join(decoded_seq)
+
+    def next_batch(self, batch_size):
+        """ Generate a minibatch of training data: inputs and outputs.
+        Creates an array of size batch_size x max_seq_length x alphabet_len
+        and fills it with the one-hot encoded versions of batch_size number of
+        random sequences that haven't been used yet in this epoch. For sequences
+        that are less than max_seq_len, they are just padded with zeros. Then the
+        training outputs are just these matrices but without the first letter and
+        with an addition 'end_token' at the end
+        """
+
+        mb = np.zeros((batch_size, self.max_seq_len, self.alphabet_len))
+        output_mb = np.zeros((batch_size, self.max_seq_len, self.alphabet_len))
+
+        for i in range(batch_size): 
+            # Check if there are any seqs left in this epoch 
             if len(self.train_seq_ids) > 0:
+                # Choose a random sequence
                 seq_idx = random.randint(0,len(self.train_seq_ids)-1)
-                x = self._idx_to_one_hot(self.seqs[self.train_seq_ids[seq_idx]])
-                mb[i] = x
-                del self.train_seq_ids[seq_idx] # Pop the seq off so that you don't use it again
+                seq = self.seqs[self.train_seq_ids[seq_idx]]
+                print "THIS IS THE SEQ: ", seq
+
+                mb[i] = self.str_to_one_hot(seq)
+                output_mb[i] = self.str_to_one_hot(self.convert_to_output(seq))
+
+                # Pop the seq off so that you don't use it again
+                del self.train_seq_ids[seq_idx]
             else:
+                # Incase you don't get to the end of the batch size, this will trim off the ones
+                # you didn't get to (TODO: check if this is necessary)
+                mb = mb[:i]
+                output_mb = output_mb[:i]
+
+                # Set up the training ids for the next epoch
                 self.reset_train_set()
                 break
-        
-        # Incase you don't get to the end of the batch size, this will trim off the ones
-        # you didn't get to (TODO: check if this is necessary)
-        mb = mb[:i]
-        
-        # First trim the first aa off, then add an extra 0 for 'end character'
-        # TOO: Should this character actually have its own character aka be 0
-        # one hot encoded or just be a 0 vector? Rn it's just a 0 vector
-        output_mb = np.concatenate((mb[:,1:,:],
-                                    np.zeros((mb.shape[0],1,mb.shape[2]))), axis=1)
 
         return mb, output_mb
-  
+
+
     @property
     def test_data(self):
         mb = np.zeros((self.test_size, self.max_seq_len, self.alphabet_len))
-        for i, seq_id in enumerate(self.test_seq_ids):
-            mb[i] = self._idx_to_one_hot(self.seqs[seq_id])
+        output_mb = np.zeros((self.test_size, self.max_seq_len, self.alphabet_len))
 
-        output_mb = np.concatenate((mb[:,1:,:],
-                                    np.zeros((mb.shape[0],1,mb.shape[2]))), axis=1)
+        for i, seq_id in enumerate(self.test_seq_ids):
+            mb[i] = self.str_to_one_hot(self.seqs[seq_id])
+            output_mb[i] = self.str_to_one_hot(convert_input_to_output(self.seqs[seq_id]))
 
         return mb, output_mb
