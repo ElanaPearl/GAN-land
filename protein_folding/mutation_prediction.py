bf55379b08c08216911644fdec3ffa1271946b43
ElanaSimon95 <ElanaSimon95@gmail.com>
2017-07-06 22:20:27 -0400

    mutation prediction werks

--- protein_folding/mutation_prediction.py
+++ protein_folding/mutation_prediction.py
@@ -1,21 +1,26 @@
 import tensorflow as tf
-from model_w_label import MultipleSequenceAlignment, TEST_ALIGN_ID
-from LSTM import VariableSequenceLabelling
-import os
-import random
 import numpy as np
+import random
+import os
 
-log_path = './model_logs/LSTM_2017-07-05_15-01-12'
+from model_w_label import MultipleSequenceAlignment
+from LSTM import LSTM
+import tools
+
+gene = 'PABP'
+run_time = '2017-07-06_17-22-08'
+
+log_path = os.path.join('model_logs', gene, run_time)
 
 print "Getting MSA"
-MSA = MultipleSequenceAlignment(TEST_ALIGN_ID, test_ids_path=os.path.join(log_path,'test_ids.pkl'))
+MSA = MultipleSequenceAlignment(gene, run_time, seq_limit=1000)
 
 
-data = tf.placeholder(tf.float32, [None, MSA.max_seq_len, MSA.alphabet_len], name='data')
-target = tf.placeholder(tf.float32, [None, MSA.max_seq_len, MSA.alphabet_len], name='target')
+data = tf.placeholder(tf.float32, [None, MSA.max_seq_len, tools.alphabet_len], name='data')
+target = tf.placeholder(tf.float32, [None, MSA.max_seq_len, tools.alphabet_len], name='target')
 
 print "Setting up graph"
-model = VariableSequenceLabelling(data, target)
+model = LSTM(data, target, MSA.seed_weights)
 
 
 sess = tf.Session()
@@ -26,38 +31,35 @@ saver = tf.train.Saver()
 print "Restoring model"
 saver.restore(sess, tf.train.latest_checkpoint(os.path.join(log_path,'checkpoints')))
 
-wildtype_d, wildtype_t = MSA.next_batch(5, test=True)
 
 
-print "Making mutations"
-mutated_d = np.copy(wildtype_d)
-mutated_t = np.copy(wildtype_t)
+wildtype_d = np.reshape(MSA.str_to_one_hot(MSA.ref_seq), (1, MSA.max_seq_len, tools.alphabet_len))
+wildtype_t = np.reshape(MSA.str_to_one_hot(MSA.ref_seq[1:] + tools.END_TOKEN), (1, MSA.max_seq_len, tools.alphabet_len))
 
-mutations = np.zeros((wildtype_d.shape[0],2))
 
-# Add random mutations to each mutation
-for seq_idx in range(wildtype_d.shape[0]):
-    # Choose random idx to randomly mutate (anything but first/last position)
-    idx_to_change = random.choice(range(wildtype_d.shape[1]-1))
+# Create all possible mutations of the reference sequence
+mutated_d = []
+mutated_t = []
+for i in range(len(MSA.ref_seq)):
+    for j in tools.alphabet:
+        mutated_seq = list(MSA.ref_seq)
+        mutated_seq[i] = j
+        mutated_d.append(MSA.str_to_one_hot(''.join(mutated_seq)))
+        mutated_t.append(MSA.str_to_one_hot(''.join(mutated_seq[1:]) + tools.END_TOKEN))
 
-    old_val = np.argmax(wildtype_d[seq_idx,idx_to_change,:])
-    new_val = random.choice(range(wildtype_d.shape[2]))
+mutated_d = np.reshape(mutated_d, (MSA.max_seq_len*tools.alphabet_len, MSA.max_seq_len, tools.alphabet_len))
 
-    mutated_d[seq_idx,idx_to_change,old_val] = 0
-    mutated_d[seq_idx,idx_to_change,new_val] = 1
+print "Calculating Mutation Probabilities"
+wildtype_seed_prob = [MSA.seed_weights[i] for i in np.argmax(wildtype_d[:,0,:],1)]
+mutated_seed_prob = [MSA.seed_weights[i] for i in np.argmax(mutated_d[:,0,:],1)]
 
-    # The idx to change is subtracted by 1 because the target is offset by 1
-    mutated_t[seq_idx,idx_to_change-1,old_val] = 0
-    mutated_t[seq_idx,idx_to_change-1,new_val] = 1
-
-    mutations[seq_idx] = [old_val, new_val]
-
-#mutated_first_aa = np.argmax()
-
-print "Getting mutation predictions"
-mutated_prob = sess.run(model.cross_entropy, {data: mutated_d, target: mutated_t})
 wildtype_prob = sess.run(model.cross_entropy, {data: wildtype_d, target: wildtype_t})
+mutated_prob = sess.run(model.cross_entropy, {data: mutated_d, target: mutated_t})
 
-#mutation_prob = (mutated_prob + MSA.seed_weights[mutated_d[:,0,:]])
+mutation_preds = (mutated_prob + mutated_seed_prob) - (wildtype_prob + wildtype_seed_prob)
 
-import pdb; pdb.set_trace()
+mutation_preds = np.reshape(mutation_preds,(MSA.max_seq_len, tools.alphabet_len))
+
+print "Saving!"
+np.savetxt(os.path.join(log_path,'mutation_preds.csv'), mutation_preds, delimiter=',')
+
